9bdaecaedfba1ba53e75776ec2d0bfcc33474a9e
bdehapiot <b.dehapiot@gmail.com>
2024-02-14 12:52:20 +0100

    update

--- analysis.py
+++ analysis.py
@@ -1,6 +1,53 @@
 #%% Imports -------------------------------------------------------------------
 
+import pickle
+import numpy as np
 from skimage import io
+from pathlib import Path
+from scipy.ndimage import shift
 
 #%% Inputs --------------------------------------------------------------------
 
+data_path = "D:/local_Concrete/data/DIA"
+exp_name = (
+    "D1_ICONX_DoS"
+    # "D11_ICONX_DoS"
+    # "D12_ICONX_corrosion"
+    # "H9_ICONX_DoS"
+    )
+
+#%%
+
+# -----------------------------------------------------------------------------
+
+def roll_image(img_rsize):
+    idx = np.argwhere((img_rsize > 30000) == 1)
+    y0, x0 = img_rsize.shape[0] // 2, img_rsize.shape[1] // 2
+    y1, x1 = np.mean(idx, axis=0)
+    yx_shift = [y0 - y1, x0 - x1]
+    return shift(img_rsize, yx_shift, mode='wrap'), yx_shift 
+
+# -----------------------------------------------------------------------------
+
+# Open data
+stack_reg = io.imread(Path(data_path, f"{exp_name}_reg.tif"))
+stack_reg_mean = np.mean(stack_reg, axis=0)
+
+# Save 
+io.imsave(
+    Path(data_path, f"{exp_name}_stack_reg_mean.tif"),
+    stack_reg_mean.astype("float32"),
+    check_contrast=False,
+    )
+
+# # Roll stack
+# print("  Roll       :", end='')
+# t0 = time.time()
+# outputs = Parallel(n_jobs=-1)(
+#         delayed(roll_image)(img_rsize) 
+#         for img_rsize in stack_rsize
+#         )
+# stack_roll = np.stack([data[0] for data in outputs])
+# yx_shifts = [data[1] for data in outputs]
+# t1 = time.time()
+# print(f" {(t1-t0):<5.2f}s") 
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from functions import process_stacks, register_stacks
 
 #%% Parameters ----------------------------------------------------------------
 
-rsize_factor = 8 # Image size reduction factor
+rsize_factor = 4 # Image size reduction factor
 mtx_thresh_coeff = 1.0 # adjust matrix threshold
 rod_thresh_coeff = 1.0 # adjust rod threshold
 
@@ -30,7 +30,7 @@ for folder in Path(data_path).iterdir():
                 
 #%% Execute -------------------------------------------------------------------
 
-# Process stack
+# Process stacks
 stack_data = []
 for stack_path in stack_paths:
     if exp_name in stack_path.name: 
@@ -42,13 +42,13 @@ for stack_path in stack_paths:
             rod_thresh_coeff,
             )
 
-# Register stack        
-stack_rsize_reg = register_stacks(stack_data)
+# Register stacks     
+stack_reg = register_stacks(stack_data)
 
 # Save 
 io.imsave(
-    Path(data_path, f"{exp_name}_registered.tif"),
-    stack_rsize_reg.astype("float32"),
+    Path(data_path, f"{exp_name}_stack_reg.tif"),
+    stack_reg.astype("float32"),
     check_contrast=False,
     imagej=True,
     metadata={'axes': 'TZYX'},
@@ -56,83 +56,6 @@ io.imsave(
     planarconfig='contig',
     )
 
-# import pickle
-# with open(Path(data_path, f"{exp_name}_stack_data.pkl"), 'wb') as f:
-#     pickle.dump(stack_data, f)
-
-#%% Save ----------------------------------------------------------------------
-
-# from skimage import io
-
-# for data in stack_data:
-    
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_rsize.tif"),
-#         data["stack_rsize"].astype("float32"), check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_roll.tif"),
-#         data["stack_roll"].astype("float32"), check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_norm.tif"),
-#         data["stack_norm"].astype("float32"), check_contrast=False,
-#         )
-    
-#     # -------------------------------------------------------------------------
-    
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_avg_proj.tif"),
-#         data["avg_proj"].astype("float32"), check_contrast=False,
-#         )    
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_rod_mask.tif"),
-#         data["rod_mask"].astype("uint8") * 255, check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_mtx_mask.tif"),
-#         data["mtx_mask"].astype("uint8") * 255, check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_rod_EDM.tif"),
-#         data["rod_EDM"].astype("float32"), check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_mtx_EDM.tif"),
-#         data["mtx_EDM"].astype("float32"), check_contrast=False,
-#         )
-    
-#     # -------------------------------------------------------------------------
-   
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_avg_proj_3D.tif"),
-#         data["avg_proj_3D"].astype("float32"), check_contrast=False,
-#         )    
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_rod_mask_3D.tif"),
-#         data["rod_mask_3D"].astype("uint8") * 255, check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_mtx_mask_3D.tif"),
-#         data["mtx_mask_3D"].astype("uint8") * 255, check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_rod_EDM_3D.tif"),
-#         data["rod_EDM_3D"].astype("float32"), check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_mtx_EDM_3D.tif"),
-#         data["mtx_EDM_3D"].astype("float32"), check_contrast=False,
-#         )
-    
-#     # -------------------------------------------------------------------------
-    
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_obj_mask_3D.tif"),
-#         data["obj_mask_3D"].astype("uint8") * 255, check_contrast=False,
-#         )
-#     io.imsave(
-#         Path(data_path, f"{data['stack_path'].stem}_obj_labels_3D.tif"),
-#         data["obj_labels_3D"].astype("uint16"), check_contrast=False,
-#         )
-
+import pickle
+with open(Path(data_path, f"{exp_name}_stack_data.pkl"), 'wb') as f:
+    pickle.dump(stack_data, f)
